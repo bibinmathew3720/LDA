1bd39ddbbb1b89f6cd3f80392c5e758e8af43656
Bibin Mathew <bibin.m@codelynks.com>
2018-06-05 20:15:43 +0530

    One way view creation.

---
+++ LDA/LDA/Views/OneWayRound/OneWayRoundView.h
@@ -0,0 +1,13 @@
+//
+//  OneWayRoundView.h
+//  LDA
+//
+//  Created by Bibin Mathew on 6/5/18.
+//  Copyright © 2018 lda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface OneWayRoundView : UIView
+
+@end
---
+++ LDA/LDA/Views/OneWayRound/OneWayRoundView.m
@@ -0,0 +1,21 @@
+//
+//  OneWayRoundView.m
+//  LDA
+//
+//  Created by Bibin Mathew on 6/5/18.
+//  Copyright © 2018 lda. All rights reserved.
+//
+
+#import "OneWayRoundView.h"
+
+@implementation OneWayRoundView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
