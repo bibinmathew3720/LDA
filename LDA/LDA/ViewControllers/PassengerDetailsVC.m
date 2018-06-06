3fbda4b86c22e4efe3d0eae65cee50554a81fc6c
Bibin Mathew <bibin.m@codelynks.com>
2018-06-07 00:07:22 +0530

    Passenger Details View implemented.

--- LDA/LDA/ViewControllers/PassengerDetailsVC.m
+++ LDA/LDA/ViewControllers/PassengerDetailsVC.m
@@ -7,9 +7,21 @@
 //
 #import "PassengerDetailsVC.h"
 
-@interface PassengerDetailsVC ()
+@interface PassengerDetailsVC ()<UITextFieldDelegate>
 @property (weak, nonatomic) IBOutlet UIView *firstNameView;
 @property (weak, nonatomic) IBOutlet UITextField *firstNameTF;
+@property (weak, nonatomic) IBOutlet UIView *lastNameView;
+@property (weak, nonatomic) IBOutlet UITextField *lastNameTF;
+@property (weak, nonatomic) IBOutlet UIView *emailView;
+@property (weak, nonatomic) IBOutlet UITextField *emailTF;
+@property (weak, nonatomic) IBOutlet UIView *phoneView;
+@property (weak, nonatomic) IBOutlet UITextField *phoneTF;
+@property (weak, nonatomic) IBOutlet UIView *alternativePhoneView;
+@property (weak, nonatomic) IBOutlet UITextField *alternativePhoneTF;
+@property (weak, nonatomic) IBOutlet UIView *preferredFlightView;
+@property (weak, nonatomic) IBOutlet UITextField *preferredFlightTF;
+@property (weak, nonatomic) IBOutlet UIView *commentView;
+@property (weak, nonatomic) IBOutlet UITextView *commentTextView;
 
 @end
 
@@ -21,7 +33,14 @@
 }
 
 -(void)initialisation{
+    self.title = @"Passenger Details";
     [self settingBorderToView:self.firstNameView];
+    [self settingBorderToView:self.lastNameView];
+    [self settingBorderToView:self.emailView];
+    [self settingBorderToView:self.phoneView];
+    [self settingBorderToView:self.alternativePhoneView];
+    [self settingBorderToView:self.preferredFlightView];
+    [self settingBorderToView:self.commentView];
 }
 
 -(void)settingBorderToView:(UIView *)view{
@@ -32,6 +51,39 @@
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - Text Field Delegates
+
+-(BOOL)textFieldShouldReturn:(UITextField *)textField{
+    if(textField == self.firstNameTF){
+        [self.lastNameTF becomeFirstResponder];
+    }
+    else if(textField == self.lastNameTF){
+        [self.emailTF becomeFirstResponder];
+    }
+    else if(textField == self.lastNameTF){
+        [self.emailTF becomeFirstResponder];
+    }
+    else if(textField == self.emailTF){
+        [self.phoneTF becomeFirstResponder];
+    }
+    else if(textField == self.phoneTF){
+        [self.alternativePhoneTF becomeFirstResponder];
+    }
+    else if(textField == self.alternativePhoneTF){
+        [self.preferredFlightTF becomeFirstResponder];
+    }
+    else if (textField == self.preferredFlightTF){
+        [self.commentTextView becomeFirstResponder];
+    }
+    return YES;
+}
+
+#pragma mark - Actions
+
+- (IBAction)tapGestureAction:(UITapGestureRecognizer *)sender {
+    [self.view endEditing:YES];
 }
 
 /*
