2bb5669d2c4a38e4fb286b93fe3a6f88a5d07318
Bibin Mathew <bibin.m@codelynks.com>
2018-06-08 09:55:58 +0530

    Flexibility view added to project.

---
+++ LDA/LDA/Views/FlexibilityView/FlexibilityView.h
@@ -0,0 +1,13 @@
+//
+//  FlexibilityView.h
+//  LDA
+//
+//  Created by Bibin Mathew on 6/8/18.
+//  Copyright © 2018 lda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FlexibilityView : UIView
+
+@end
---
+++ LDA/LDA/Views/FlexibilityView/FlexibilityView.m
@@ -0,0 +1,21 @@
+//
+//  FlexibilityView.m
+//  LDA
+//
+//  Created by Bibin Mathew on 6/8/18.
+//  Copyright © 2018 lda. All rights reserved.
+//
+
+#import "FlexibilityView.h"
+
+@implementation FlexibilityView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
