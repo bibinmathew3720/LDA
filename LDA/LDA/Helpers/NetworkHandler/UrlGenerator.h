bf21e7bec61f98be1fc1d6745b6acf4bf6ca1081
Bibin Mathew <bibin.m@codelynks.com>
2018-06-10 03:39:48 +0530

    Service urls generated.

--- LDA/LDA/Helpers/NetworkHandler/UrlGenerator.h
+++ LDA/LDA/Helpers/NetworkHandler/UrlGenerator.h
@@ -9,75 +9,23 @@
 #import <Foundation/Foundation.h>
 
 
-//static NSString *VERANOBaseUrl =@"http://34.215.160.196/SmartInverter/index.php?"; //Test Server
-static NSString *VERANOBaseUrl =@"http://vguard.co/mobileapps/verano/index.php?"; // Production Server
+//static NSString *LDABaseUrl =@"http://34.215.160.196/SmartInverter/index.php?"; //Test Server
+static NSString *LDABaseUrl = @"http://app.luxurydiscountair.com/"; // Production Server
 
-static NSString *VERANOSubmitUrl = @"submitprofilenew";//@"submitprofile";
-static NSString *VERANOUpdateProfile = @"updateProfile";
-static NSString *VERANOLoginUrl = @"/Users/login";
-static NSString *VERANUserDetailUrl = @"Users/me";//@"me";
-static NSString *VERANORefreshToken = @"token?refresh_token=";
-static NSString *VERANOForgotPassword = @"forgotPassword";
-static NSString *VERANOGetUserFeedBack =@"getUserFeedback";
-static NSString *VERANOSubmitProductProfile = @"submitProductProfile";//@"submitProductProfilenew";
-static NSString *VERANOSubmitFanProductProfile = @"smartFan/submitProductProfileTest";
-static NSString *VERANOGetMyProducts = @"getMyProducts";
-static NSString *VERANODeleteProduct = @"deleteProduct";
-static NSString *VERANOUpdateSettingsFromMobile = @"updateSettingsFromMobile";
-static NSString *VERANOGetVeranoSettings = @"getVeranoSettings";
-static NSString *VERANOPostVeranoComplaints = @"Users/submitComplaint";
-static NSString *VERANOGetModel = @"productmodel";
-static NSString *VERANOInstallationRequest = @"installationRequest";
-static NSString *VERANOConnectProduct = @"connectproduct";
-static NSString *VERANOCustomerCareUrl = @"http://www.vguard.in/home/customer-care";
-static NSString *VERANOLogoutUrl = @"logout";
-static NSString *VERANOSubmitInverterReadings = @"submitInverterReadings";
-static NSString *InverterGetProductInfo = @"getProductInfo";
-static NSString *InverterSubmitMacId = @"submitMacId";
-static NSString *registerDevicetoken = @"registerDevice";
-static NSString *getDeviceSettings = @"getDeviceSettings";
-static NSString *complaintStatus = @"complaintStatus";
-static NSString *DistricListUrl = @"http://www.vguard.in/get-district-list?state=";
-static NSString *VERANOPrivacyPolicyUrl = @"http://vguard.co/mobileapps/smart/privacy_policy.html";
+static NSString *LDASearchUrl = @"v1/airport";
+static NSString *LDASaveTripDetails = @"v1/trip";
+static NSString *LDATermsAndConditions = @"about/terms";
 
-//Urls for Fan
-
-static NSString *getProductInfoUrlForFan = @"smartFan/getProductInfo";
-
-typedef NS_ENUM(NSInteger, VERANOURLTYPE ){
-    VERANOURLTYPESubmitProfile = 1,
-    VERANOURLTYPEUserLogin = 2,
-    VERANOURLTYPEUserDetails = 3,
-    VERANOURLTYPERefreshToken = 4,
-    VERANOURLTYPEForgotPassword = 5,
-    VERANOURLTYPEAddProduct = 6,
-    VERANOURLTYPEGetMyProducts = 7,
-    VERANOURLTYPEDeleteProduct = 8,
-    VERANOURLTYPEUpdateSettingsFromMobile =9,
-    VERANOURLTYPEGetVeranoSettings = 10,
-    VERANOURLTYPEPostVeranoComplaints = 11,
-    VERANOURLTYPEGetUSerFeedBack = 12,
-    VERANOURLTYPEGetModel = 13,
-    VERANOURLTYPEInstallationRequest = 14,
-    VERANOURLTYPEConnectProduct = 15,
-    VERANOURLTYPEUpdateProfile = 16,
-    VERANOURLTYPELogout = 17,
-    VERANOURLTYPESubmitInverterSettings = 18,
-    INVERTERURLTYPEGETPRODUCTINFO = 19,
-    INVERTERURLTYPESUBMITMACID = 20,
-    VERANOURLTYPEDistricList = 21,
-    VERANOREGISTERDEVICETOKEN = 22,
-    VERANOREGISTERComplaintStatus = 23,
-    VERANOREGISTERGetDeviceSettings = 24,
-    VERANOURLTYPEAddFanProduct = 25,
-    
-    IMAGINAURLTYPEGetFanProductInfo = 26,
+typedef NS_ENUM(NSInteger, LDAURLType ){
+    LDAURLTYPESearch = 1,
+    LDAOURLTYPESubmitTripDetails = 2,
+    LDAOURLTYPETermsAndConditions = 3,
     
 };
 
 @interface UrlGenerator : NSObject
 
 +(UrlGenerator *) sharedHandler;
-- (NSURL *)urlForRequestType:(VERANOURLTYPE) type withURLParameter:(NSString *)urlParameter;
+- (NSURL *)urlForRequestType:(LDAURLType) type withURLParameter:(NSString *)urlParameter;
 
 @end
--- LDA/LDA/Helpers/NetworkHandler/UrlGenerator.m
+++ LDA/LDA/Helpers/NetworkHandler/UrlGenerator.m
@@ -19,88 +19,18 @@
     return handler;
 }
 
-- (NSURL *)urlForRequestType:(VERANOURLTYPE) type withURLParameter:(NSString *)urlParameter {
-    NSString *baseURL = VERANOBaseUrl;
+- (NSURL *)urlForRequestType:(LDAURLType) type withURLParameter:(NSString *)urlParameter {
+    NSString *baseURL = LDABaseUrl;
     NSString *appendingUrl;
     switch (type) {
-        case VERANOURLTYPESubmitProfile:
-            appendingUrl = urlParameter;
+        case LDAURLTYPESearch:
+            appendingUrl = LDASearchUrl;
             break;
-        case VERANOURLTYPEUserDetails:
-            appendingUrl = VERANUserDetailUrl;
+        case LDAOURLTYPESubmitTripDetails:
+            appendingUrl = LDASaveTripDetails;
             break;
-        case VERANOURLTYPEUserLogin:
-            appendingUrl = VERANOLoginUrl;
-            break;
-        case VERANOURLTYPERefreshToken:
-            appendingUrl = [NSString stringWithFormat:@"%@%@",VERANORefreshToken,urlParameter];
-            break;
-        case VERANOURLTYPEForgotPassword:
-            appendingUrl = VERANOForgotPassword;
-            break;
-        case VERANOURLTYPEAddProduct:
-            appendingUrl = VERANOSubmitProductProfile;
-            break;
-        case VERANOURLTYPEGetMyProducts:
-            appendingUrl = VERANOGetMyProducts;
-            break;
-        case VERANOURLTYPEDeleteProduct:
-            appendingUrl = VERANODeleteProduct;
-            break;
-        case VERANOURLTYPEUpdateSettingsFromMobile:
-            appendingUrl = VERANOUpdateSettingsFromMobile;
-            break;
-        case VERANOURLTYPEGetVeranoSettings:
-            appendingUrl = VERANOGetVeranoSettings;
-            break;
-        case VERANOURLTYPEPostVeranoComplaints:
-            appendingUrl = VERANOPostVeranoComplaints;
-            break;
-        case VERANOURLTYPEGetUSerFeedBack:
-            appendingUrl = VERANOGetUserFeedBack;
-            break;
-        case VERANOURLTYPEGetModel:
-            appendingUrl = VERANOGetModel;
-            break;
-        case VERANOURLTYPEInstallationRequest:
-            appendingUrl = VERANOInstallationRequest;
-            break;
-        case  VERANOURLTYPEConnectProduct :
-            appendingUrl = VERANOConnectProduct;
-            break;
-        case  VERANOURLTYPEUpdateProfile :
-            appendingUrl = VERANOUpdateProfile;
-            break;
-        case VERANOURLTYPELogout:
-            appendingUrl = VERANOLogoutUrl;
-            break;
-        case VERANOURLTYPESubmitInverterSettings:
-            appendingUrl = VERANOSubmitInverterReadings;
-            break;
-        case INVERTERURLTYPEGETPRODUCTINFO:
-            appendingUrl = InverterGetProductInfo;;
-            break;
-        case INVERTERURLTYPESUBMITMACID:
-            appendingUrl = InverterSubmitMacId;;
-            break;
-        case VERANOURLTYPEDistricList:
-            appendingUrl = [NSString stringWithFormat:@"%@%@",DistricListUrl,urlParameter];
-            return [NSURL URLWithString:[appendingUrl stringByAddingPercentEscapesUsingEncoding:NSUTF8StringEncoding]];
-            break;
-        case VERANOREGISTERDEVICETOKEN:
-            appendingUrl = registerDevicetoken;;
-            break;
-        case VERANOREGISTERComplaintStatus:
-            appendingUrl = complaintStatus;;
-            break;
-        case VERANOREGISTERGetDeviceSettings:
-            appendingUrl = getDeviceSettings;;
-            break;
-        case VERANOURLTYPEAddFanProduct:
-            appendingUrl = VERANOSubmitFanProductProfile;
-            break;
-        case IMAGINAURLTYPEGetFanProductInfo:
-            appendingUrl = getProductInfoUrlForFan;
+        case LDAOURLTYPETermsAndConditions:
+            appendingUrl = LDATermsAndConditions;
             break;
         default:
             break;
