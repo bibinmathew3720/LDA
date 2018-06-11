52c1fdc435e983835601262f1b775f5a07b2cb95
Bibin Mathew <bibin.m@codelynks.com>
2018-06-11 13:49:29 +0530

    IQ keyboard added to project.

--- LDA/LDA/AppDelegate.m
+++ LDA/LDA/AppDelegate.m
@@ -7,6 +7,7 @@
 //
 #import "Constants.h"
 #import "AppDelegate.h"
+#import "IQKeyboardManager.h"
 
 @interface AppDelegate ()
 
@@ -18,6 +19,7 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
     [self initNavigationBarAppearence];
+    IQKeyboardManager.sharedManager.enable = YES;
     return YES;
 }
 
